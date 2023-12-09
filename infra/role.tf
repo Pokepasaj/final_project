e38807edbedfd5e0867679f187745f711d7bdb38
Kostadin <pokepasaj@gmail.com>
2023-12-09 16:18:36 +0200

    role.tf changes

--- infra/role.tf
+++ infra/role.tf
@@ -1,31 +1,32 @@
 resource "aws_iam_role" "role" {
-  name = "kind-role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Sid    = ""
-        Principal = {
-          Service = "ec2.amazonaws.com"
-        }
+  name               = "kubernetes-role"
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
       },
-    ]
-  })
-
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
 }
 
-data "aws_iam_policy" "admin" {
+data "aws_iam_policy" "admin_access" {
   arn = "arn:aws:iam::aws:policy/AdministratorAccess"
 }
-resource "aws_iam_role_policy_attachment" "admin-attach" {
+
+resource "aws_iam_role_policy_attachment" "admin-role-policy-attach" {
   role       = aws_iam_role.role.name
-  policy_arn = data.aws_iam_policy.admin.arn
+  policy_arn = data.aws_iam_policy.admin_access.arn
 }
 
 resource "aws_iam_instance_profile" "profile" {
-  name = "kind-profile"
+  name = "k8s-profile"
   role = aws_iam_role.role.name
 }
